7546851e786333add59433a07eb2459261432997
Franco Balich <franco.balich@gmail.com>
2024-11-11 13:04:05 -0300

    feat: add vertical check

--- level-1/challenge.py
+++ level-1/challenge.py
@@ -23,16 +23,6 @@ def is_mutant(dna):
                 if sequence_count > 1:
                     return True
 
-    for row in range(n - 3):
-        for col in range(n - 3):
-            if dna[row][col] == dna[row+1][col+1] == dna[row+2][col+2] == dna[row+3][col+3]:
-                sequence_count += 1
-                if sequence_count > 1:
-                    return True
-            if dna[row][col+3] == dna[row+1][col+2] == dna[row+2][col+1] == dna[row+3][col]:
-                sequence_count += 1
-                if sequence_count > 1:
-                    return True
     return False
 
 resp=is_mutant(dna)
